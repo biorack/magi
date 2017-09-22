ab099c7bc04c731b9c013c9bcef586ee5dcd7e74
Onur Erbilgin <oerbilgin@lbl.gov>
2017-09-22 14:35:07 -0700

    polishing

--- magi_job/emailer.py
+++ magi_job/emailer.py
@@ -12,40 +12,38 @@ run_jobs = utils.retrieve_jobs(sift=[('runflag', 'True')])
 run_jobs = utils.adjust_file_paths(run_jobs)
 
 for job in run_jobs:
-	job_path = utils.get_job_dir(job)
-	admin_path = os.path.join(magi_task_root, job_path, 'admin')
-	job_link = os.path.join(base_url, 'jobs/?id=%s' % (job['pk']))
-	if os.path.isfile(os.path.join(admin_path, 'start_time.txt')):
-		if not os.path.isfile(os.path.join(admin_path, 'start_email.txt')):
-			subj = 'MAGI Job Started!'
-			msg = ''
-			msg += 'Your MAGI job has started!\n'
-			msg += '%s\n\n' % (job_link)
+    job_path = utils.get_job_dir(job)
+    admin_path = os.path.join(magi_task_root, job_path, 'admin')
+    job_link = os.path.join(base_url, 'jobs/?id=%s' % (job['pk']))
+    if os.path.isfile(os.path.join(admin_path, 'start_time.txt')):
+        if not os.path.isfile(os.path.join(admin_path, 'start_email.txt')):
+            subj = 'MAGI Job Started!'
+            msg = ''
+            msg += 'Your MAGI job has started!\n'
+            msg += '%s\n\n' % (job_link)
             msg += "You can monitor your job's progress by looking at the log files:\n"
             msg += 'OUTPUT: %s\n' % (os.path.join(base_url, 'files//processed/%s/' % (job_path), 'log_out.txt'))
             msg += 'ERROR: %s\n\n' % (os.path.join(base_url, 'files//processed/%s/' % (job_path), 'log_err.txt'))
             msg += 'If you have any questions, please contact us by replying to this email.\n'
 
-			utils.email_user(job['fields']['email'], subj, msg)
+            utils.email_user(job['fields']['email'], subj, msg)
 
-			subprocess.call(['date', '>', 'start_email.txt'],
-				cwd='%s' % (admin_path))
-			
-			print 'emailed %s start email for job %s' % (job['fields']['email'], job['pk'])
-			continue
-	result_files = glob.glob(os.path.join(magi_task_root, job_path, '*results.csv'))
-	if len(result_files) > 0:
-		if not os.path.isfile(os.path.join(admin_path, 'end_email.txt')):
-			subj = 'MAGI Job is Done!'
-			msg = ''
-			msg += 'Your MAGI job is done!\n'
-			msg += 'See your results here: %s\n\n' % (job_link)
-			msg += 'If you have any questions, please contact us by replying to this email.\n'
+            subprocess.call(['touch', '%s/start_email.txt' % (admin_path)])
+            
+            # print 'emailed %s start email for job %s' % (job['fields']['email'], job['pk'])
+            continue
+    result_files = glob.glob(os.path.join(magi_task_root, job_path, '*results.csv'))
+    if len(result_files) > 0:
+        if not os.path.isfile(os.path.join(admin_path, 'end_email.txt')):
+            subj = 'MAGI Job is Done!'
+            msg = ''
+            msg += 'Your MAGI job is done!\n'
+            msg += 'See your results here: %s\n\n' % (job_link)
+            msg += 'If you have any questions, please contact us by replying to this email.\n'
 
-			utils.email_user(utils.email_user(job['fields']['email'], subj, msg))
+            utils.email_user(job['fields']['email'], subj, msg)
 
-			subprocess.call(['date', '>', 'end_email.txt'],
-				cwd='%s' % (admin_path))
+            subprocess.call(['touch', '%s/end_email.txt' % (admin_path)])
 
-			print 'emailed %s end email for job %s' % (job['fields']['email'], job['pk'])
-			continue
+            # print 'emailed %s end email for job %s' % (job['fields']['email'], job['pk'])
+            continue
