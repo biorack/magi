83a8d08d2782f297fa006f12c33554cda642ba88
Onur Erbilgin <oerbilgin@lbl.gov>
2017-09-29 13:16:35 -0700

    setup options

--- setup.py
+++ setup.py
@@ -1,60 +1,15 @@
 import os
 import subprocess
+import argparse
 
-repo_path = os.getcwd()
-
-fname = raw_input('USER INPUT: Settings Name (leave blank for default): ')
-
-# step one: extract the db tarball
-print 'Extracting database files...'
-db_dir = os.path.join(repo_path, 'workflow', 'database')
-# wipe out existing db directory
-if os.path.isdir(db_dir):
-    subprocess.call(['rm', '-r', db_dir])
-os.makedirs(db_dir)
-for zipfile in ['blastdb.zip', 'pickle_files.zip']:
-    cmd = ['unzip', '../%s' %(zipfile)]
-    subprocess.call(
-        cmd,
-        cwd = os.path.join(repo_path, 'workflow/database')
-        )
-print 'Done'
-
-# step two: make local_settings.py file
-
-if fname == '':
-    fname = 'user_settings'
-
-lines = ["import os\n\n"]
-lines.append("repo_location = '%s'\n\n" % (repo_path))
-lines.append("blastbin =          os.path.join(repo_location, 'workflow/blastbin')\n")
-lines.append("\n")
-lines.append("refseq_path =       os.path.join(repo_location, 'workflow/database/mrs_reaction_filtered_refseq_db_newrxns_actinofixed.pkl')\n")
-lines.append("refseq_db =         os.path.join(repo_location, 'workflow/database/mrs_reaction_filtered_refseq_db_fasta_blastdb_actinofix')\n")
-lines.append("mrs_reaction_path = os.path.join(repo_location, 'workflow/database/mrs_reaction_newrxns_added_actinofix.pkl')\n")
-lines.append("compounds_df =      os.path.join(repo_location, 'workflow/database/unique_compounds_groups_magi.pkl')\n")
-lines.append("mst_path =          os.path.join(repo_location, 'workflow/database/graph.pkl')\n")
-lines.append("chemnet_pickle =    os.path.join(repo_location, 'workflow/database/compound_groups.pkl')\n")
-lines.append("c2r =               os.path.join(repo_location, 'workflow/database/c2r.pkl')\n")
-lines.append("\n")
-lines.append("magi_results_storage = os.path.join(repo_location, 'outputs')\n")
-
-with open('local_settings/%s.py' %(fname), 'w') as f:
-    for line in lines:
-        f.write(line)
-with open('local_settings/local_settings.py', 'a') as f:
-    f.write("SETTINGS_FILE= '%s'\n" % (fname))
-print 'Successfully wrote local settings files'
-
-# steps 3 and 4: change localsettings paths
-def change_localsettings_path(fname):
+def change_localsettings_path(fname, repo_path):
     """
     Requires the sys.path.insert to be all on one line
     Requires "from local_settings import local_settings as settings_loc"
         to immediately follow sys.path.insert
     overwrites existing file!
     """
-    
+
     with open(fname, 'r') as f:
         lines = f.readlines()
     for i, line in enumerate(lines):
@@ -67,11 +22,78 @@ def change_localsettings_path(fname):
     with open(fname, 'w') as f:
         f.write(''.join(lines))
     print 'Successfully adjusted %s' % (fname)
-files = [
-    'workflow/workflow_helpers.py',
-    'workflow/magi_workflow_20170519.py'
-]
-for fname in files:
-    change_localsettings_path(fname)
 
-print 'Setup Successful!'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='full setup', action='store_true')
+    parser.add_argument('-d', help='database only', action='store_true', default=False)
+    parser.add_argument('-p', help='paths only', action='store_true', default=False)
+    args = parser.parse_args()
+
+    repo_path = os.getcwd()
+
+    if args.d or args.p:
+        args.f = False
+    else:
+        args.f = True
+
+    if args.f:
+        fname = raw_input('USER INPUT: Settings Name (leave blank for default): ')
+
+    if args.f or args.d:
+        # step one: extract the db tarball
+        print 'Extracting database files...'
+        db_dir = os.path.join(repo_path, 'workflow', 'database')
+        # wipe out existing db directory
+        if os.path.isdir(db_dir):
+            subprocess.call(['rm', '-r', db_dir])
+        os.makedirs(db_dir)
+        for zipfile in ['blastdb.zip', 'pickle_files.zip']:
+            cmd = ['unzip', '../%s' %(zipfile)]
+            subprocess.call(
+                cmd,
+                cwd = os.path.join(repo_path, 'workflow/database')
+                )
+        print 'Done'
+
+    if args.f:
+        # step two: make local_settings.py file
+
+        if fname == '':
+            fname = 'user_settings'
+
+        lines = ["import os\n\n"]
+        lines.append("repo_location = '%s'\n\n" % (repo_path))
+        lines.append("blastbin =          os.path.join(repo_location, 'workflow/blastbin')\n")
+        lines.append("\n")
+        lines.append("refseq_path =       os.path.join(repo_location, 'workflow/database/mrs_reaction_filtered_refseq_db_newrxns_actinofixed.pkl')\n")
+        lines.append("refseq_db =         os.path.join(repo_location, 'workflow/database/mrs_reaction_filtered_refseq_db_fasta_blastdb_actinofix')\n")
+        lines.append("mrs_reaction_path = os.path.join(repo_location, 'workflow/database/mrs_reaction_newrxns_added_actinofix.pkl')\n")
+        lines.append("compounds_df =      os.path.join(repo_location, 'workflow/database/unique_compounds_groups_magi.pkl')\n")
+        lines.append("mst_path =          os.path.join(repo_location, 'workflow/database/graph.pkl')\n")
+        lines.append("chemnet_pickle =    os.path.join(repo_location, 'workflow/database/compound_groups.pkl')\n")
+        lines.append("c2r =               os.path.join(repo_location, 'workflow/database/c2r.pkl')\n")
+        lines.append("\n")
+        lines.append("magi_results_storage = os.path.join(repo_location, 'outputs')\n")
+
+        with open('local_settings/%s.py' %(fname), 'w') as f:
+            for line in lines:
+                f.write(line)
+        with open('local_settings/local_settings.py', 'a') as f:
+            f.write("SETTINGS_FILE= '%s'\n" % (fname))
+        print 'Successfully wrote local settings files'
+
+    # steps 3 and 4: change localsettings paths
+    if args.f or args.p:
+        files = [
+            'workflow/workflow_helpers.py',
+            'workflow/magi_workflow_20170519.py',
+            'magi_job/utils.py'
+        ]
+        for fname in files:
+            change_localsettings_path(fname, repo_path)
+
+    print 'Setup Done!'
+
+if __name__ == '__main__':
+    main()
