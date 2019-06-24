51338e46f4fa8e16ee7a285f3855c5e44841ad1c
pasteur Pseudo User <pasteur@cori08.nersc.gov>
2019-06-24 10:24:04 -0700

    added M+

--- magi_job/utils.py
+++ magi_job/utils.py
@@ -667,7 +667,7 @@ def mz_neutral_transform(val, adduct, transform='neutralize'):
       if neutralize, neutralizes 'val' by subtracting adduct
       if ionize, ionizes 'val' by adding adduct
     list of acceptible adducts:
-      M+H, M+NH4, M+Na, M+CH3OH+H, M+K, M+ACN+H, M+2Na-H,
+      M+, M+H, M+NH4, M+Na, M+CH3OH+H, M+K, M+ACN+H, M+2Na-H,
       M+IsoProp+H, M+ACN+Na, M+2K-H, M+DMSO+H, M+2ACN+H,
       M+IsoProp+Na+H, 2M+H, 2M+NH4, 2M+Na, 2M+K, 2M+ACN+H,
       2M+ACN+Na, M+3H, M+2H+Na, M+H+2Na, M+3Na, M+2H, M+H+NH4,
@@ -676,7 +676,7 @@ def mz_neutral_transform(val, adduct, transform='neutralize'):
       M-2H, M-H2O-H, M+Na-2H, M+K-2H, M+Br, M+TFA-H
     """
     acceptible_adducts = [
-    'M+H', 'M+NH4', 'M+Na', 'M+CH3OH+H', 'M+K', 'M+ACN+H', 'M+2Na-H',
+    'M+','M+H', 'M+NH4', 'M+Na', 'M+CH3OH+H', 'M+K', 'M+ACN+H', 'M+2Na-H',
     'M+IsoProp+H', 'M+ACN+Na', 'M+2K-H', 'M+DMSO+H', 'M+2ACN+H',
     'M+IsoProp+Na+H', '2M+H', '2M+NH4', '2M+Na', '2M+K', '2M+ACN+H',
     '2M+ACN+Na', 'M+3H', 'M+2H+Na', 'M+H+2Na', 'M+3Na', 'M+2H', 'M+H+NH4',
@@ -687,6 +687,7 @@ def mz_neutral_transform(val, adduct, transform='neutralize'):
 
     # M + N
     simple = {
+      'M+': 0.0000,
       'M+H': 1.007276,
       'M+NH4': 18.033823,
       'M+Na': 22.989218,
