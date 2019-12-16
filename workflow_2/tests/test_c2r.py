66fb7ece83222dec24e118f6ee2e4e3b7f18ae15
Hanneke Leegwater <hanneke@leegwater.com>
2019-12-16 14:25:07 -0800

    Update workflow c2r test file

--- workflow_2/tests/test_c2r.py
+++ workflow_2/tests/test_c2r.py
@@ -10,11 +10,11 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 
 # Read retro rules database
-@pytest.mark.parametrize("diameter", [2,16])
-def test_read_retro_rules_db(diameter):
+@pytest.mark.parametrize("diameter, expected_len", [(2,10),(16,5)])
+def test_read_retro_rules_db(diameter,expected_len):
     db_path = os.path.join(os.path.dirname(__file__), "./retro_rules_tiny.tsv")
     retro_rules = magi.read_retro_rules(db_path, diameter)
-    assert retro_rules.shape == (5,19)
+    assert retro_rules.shape == (expected_len,19)
 
 # Mol from SMILES
 @pytest.mark.parametrize("smiles", ['Cc1ccccc1', 'Cc1ncccc1'])
@@ -54,10 +54,39 @@ def test_compound_to_reaction():
     assert c2r["Reaction_ID"][0] == expected
 
 # Test find matching reactions
-@pytest.mark.parametrize("smile, expected", 
+@pytest.mark.parametrize("smiles, expected", 
                         [("CCC=O", True),
                         ("CCCO", False)])
-def test_find_matching_reactions(smile, expected):
-    molecule = magi.mol_from_smiles(smile)
+def test_mol_matches_reaction(smiles, expected):
+    molecule = magi.mol_from_smiles(smiles)
     reaction = AllChem.ReactionFromSmarts("[CH](=[O])-[CH2]-[CH3]>>[CH2](-[OH])-[CH](-[CH3])-[OH]")
-    assert magi.find_matching_reactions(molecule, reaction) == expected
+    assert magi.mol_matches_reaction(molecule, reaction) == expected
+
+# Test desalting and neutralizing
+@pytest.mark.parametrize("smiles, expected", 
+[('C(CCN)CC(C(=O)O)N.Cl','NCCCCC(N)C(=O)O'),
+('C(CCN)CC(C(=O)O)N','NCCCCC(N)C(=O)O'),
+('CC(=O)[O-]','CC(=O)O'),
+('CC(=O)O','CC(=O)O'),
+('C(CC[NH3+])CC(C(=O)O)N.[Cl-]','NCCCCC(N)C(=O)O'),
+('CC(=O)[O-].[Cl-]','CC(=O)O')])
+def test_desalt_canonicalize_and_neutralize_smiles(smiles, expected):
+    result = magi.desalt_canonicalize_and_neutralize_smiles(smiles)
+    assert result == expected
+
+# Test read compounds data 
+def test_read_compounds_data():
+    df = magi.read_compounds_data(os.path.join(os.path.dirname(__file__), "input_to_prep.csv"))
+    assert df.shape == (6,2)
+
+# Test tautomer canonicalization
+@pytest.mark.parametrize("smiles, expected", 
+[('C/C(O)=N/C','CNC(C)=O'),
+('C/C(O)=C/C','CCC(C)=O'),
+('N=C1C=CC=CN1','N=c1cccc[nH]1'),
+('O/C1=N/CCCCCC1','O=C1CCCCCCN1')])
+def test_canonicalize_tautomer(smiles, expected):
+    mol = magi.mol_from_smiles(smiles)
+    result = magi.canonicalize_tautomer(mol)
+    result = Chem.MolToSmiles(result)
+    assert result == expected
