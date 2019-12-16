c7450d2efe64ff9d2441ea8427819651e0afa196
Hanneke Leegwater <hanneke@leegwater.com>
2019-12-16 14:27:18 -0800

    Update in run_magi.sh
    To prevent terminal from closing when an error occurs

--- run_magi.sh
+++ run_magi.sh
@@ -26,14 +26,14 @@ echo "Starting MAGI at $(date)"
 python $magi_path/workflow/magi_workflow_gene_to_reaction.py --fasta $fasta_file --compounds $compounds_file --output $output_directory --cpu_count $cpu_count > $logfile_name 2> $error_log_name
 if [ $? -eq 0 ]; then # Check if previous MAGI run did not fail
 python $magi_path/workflow/magi_workflow_accurate_mass_search.py --not_first_script --output $output_directory >> $logfile_name 2>> $error_log_name
-else exit 1; fi
-if [ $? -eq 0 ]; then
+else touch $output_directory/incomplete; fi
+if [ $? -eq 0 ] && [ ! -f $output_directory/incomplete ]; then
 python $magi_path/workflow/magi_workflow_compound_to_reaction.py --not_first_script --output $output_directory >> $logfile_name 2>> $error_log_name
-else exit 1; fi
-if [ $? -eq 0 ]; then
+else touch $output_directory/incomplete; fi
+if [ $? -eq 0 ] && [ ! -f $output_directory/incomplete ]; then
 python $magi_path/workflow/magi_workflow_reaction_to_gene.py --not_first_script --output $output_directory >> $logfile_name 2>> $error_log_name
-else exit 1; fi
-if [ $? -eq 0 ]; then
+else touch $output_directory/incomplete; fi
+if [ $? -eq 0 ] && [ ! -f $output_directory/incomplete ]; then
 python $magi_path/workflow/magi_workflow_scoring.py --not_first_script --output $output_directory >> $logfile_name 2>> $error_log_name
-else exit 1; fi
+else touch $output_directory/incomplete; fi
 echo "Done"
