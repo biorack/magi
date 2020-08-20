e97ed710ef7f4626c14e3ce40dc2bc7cad803c39
Hanneke Leegwater <hanneke@leegwater.com>
2020-08-20 19:59:42 +0200

    Delete run_compound_to_reaction.sh

--- workflow_2/run_compound_to_reaction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run new compound to reaction
-python compound_to_reaction.py \
---compounds /global/cscratch1/sd/leegwate/magi_2/EMA_HILICz-v11_Annotations20180824_with_SMILES.csv \
---output ./output \
---retro_rules /global/cscratch1/sd/leegwate/magi_2/retrorules_rr02_flat_all.tsv
